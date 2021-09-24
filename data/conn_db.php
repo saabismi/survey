6b27ef0420509a81194f46896db0f71ba9cf72a6
saabismi <vilho.syvahuoko@gmail.com>
2021-09-24 14:38:09 +0300

    remove conn_db.php as it contains password info

--- data/conn_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "yourserver";
-$dbUsername = "kayttajanimi";
-$dbPassword = "salasana";
-$dbName = "kyselysofta";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
